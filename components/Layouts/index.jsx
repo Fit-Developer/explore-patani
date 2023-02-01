be21c0c6fddb7eefdaa34b24c39909f579c3ff5f
Fitri <fitri@delightsolution.com>
2023-02-01 16:50:06 +0700

    banner created

---
+++ components/Layouts/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Index({ children }) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
